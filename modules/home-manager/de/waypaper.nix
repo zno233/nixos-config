7e2a56879df33fa4f77164406f4b868b5727268f
zno233 <2393567502@qq.com>
2025-11-25 13:07:24 +0800

    add a nice anime wallpaper

--- modules/home-manager/de/waypaper.nix
+++ modules/home-manager/de/waypaper.nix
@@ -7,7 +7,7 @@
     language = en
     folder = ~/Pictures/wallpapers/others
     monitors = All
-    wallpaper = ~/Pictures/wallpapers/others/red.jpg
+    wallpaper = ~/Pictures/wallpapers/others/Kita.png
     backend = swww
     fill = fill
     sort = name
