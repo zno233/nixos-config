1023ea4b70e667e5b732cecd2f33a1ed70551f3a
zno233 <2393567502@qq.com>
2025-12-01 16:18:02 +0800

    add nix.gc

--- modules/nixos/oldBootloader.nix
+++ modules/nixos/oldBootloader.nix
@@ -26,4 +26,10 @@
     
     plymouth.enable = true; 
   };
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
 }
