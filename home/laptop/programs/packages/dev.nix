29201ec1a31b5d5aee0c32262a1b422a90d76395
zno233 <2393567502@qq.com>
2025-12-12 17:07:27 +0800

    add some python lib

--- home/laptop/programs/packages/dev.nix
+++ home/laptop/programs/packages/dev.nix
@@ -25,16 +25,28 @@
       # llm
       # llm-ollama
       # llm-gemini
-      
+
+      # 核心工具
       ipython
       jupyterlab
+  
+     # 数据处理与可视化
       matplotlib
       numpy
       pandas
-      mlxtend
       seaborn
+  
+      # 机器学习与数据科学
+      scikit-learn 
+      xgboost
+      joblib
+      imbalanced-learn
+  
+      # 其他
+      mlxtend
       opencv4
     ]))  
+    
     ##java
     jdk
   ];
