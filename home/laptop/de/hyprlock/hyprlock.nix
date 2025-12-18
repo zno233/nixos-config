132a5a9216a1159b7126d6c8ab655439b1927739
zno233 <2393567502@qq.com>
2025-12-18 13:21:37 +0800

    fix the problem of powe-menu.sh

--- home/laptop/de/hyprlock/hyprlock.nix
+++ home/laptop/de/hyprlock/hyprlock.nix
@@ -15,6 +15,14 @@
     # 而不是先复制到 Store 再链接。
     force = true;
   };
+
+  xdg.configFile."hypr/hyprlock.conf" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "/home/zno/zno-config/home/laptop/de/hyprlock/hyprlock.conf";
+    # 关键选项：告诉 Home Manager 创建一个直接链接到源文件
+    # 而不是先复制到 Store 再链接。
+    force = true;
+  };
  
 }
 
