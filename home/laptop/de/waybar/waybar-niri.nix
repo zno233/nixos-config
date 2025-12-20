0bc0227a339d1a350558aec7bcdab5a64bd634f4
zno233 <2393567502@qq.com>
2025-12-20 11:37:44 +0800

    use gtk theme as tray menu

--- home/laptop/de/waybar/waybar-niri.nix
+++ home/laptop/de/waybar/waybar-niri.nix
@@ -1,10 +1,28 @@
-{ ... }:
+{ 
+  config,
+  ...
+}:
 {
   programs.waybar = {
     enable = true;
   };
-  xdg.configFile."waybar/config.jsonc".source = ./config.jsonc;
-  xdg.configFile."waybar/style.css".source = ./style.css;
+
+  xdg.configFile."waybar/config.jsonc" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "/home/zno/zno-config/home/laptop/de/waybar/config.jsonc";
+    # 关键选项：告诉 Home Manager 创建一个直接链接到源文件
+    # 而不是先复制到 Store 再链接。
+    force = true;
+  };
+
+  xdg.configFile."waybar/style.css" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "/home/zno/zno-config/home/laptop/de/waybar/style.css";
+    # 关键选项：告诉 Home Manager 创建一个直接链接到源文件
+    # 而不是先复制到 Store 再链接。
+    force = true;
+  };
+
   xdg.configFile."waybar/scripts".source = ./scripts;
   
 }
