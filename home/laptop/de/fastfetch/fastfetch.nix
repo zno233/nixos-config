e48c47023cca4ff4498ee403bfccf3ea17ed6428
zno233 <2393567502@qq.com>
2025-12-28 15:04:40 +0800

    add fastfetch styles

--- home/laptop/de/fastfetch/fastfetch.nix
+++ home/laptop/de/fastfetch/fastfetch.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [ fastfetch ];
 
-  xdg.configFile."fastfetch/config.jsonc".source = ./config.jsonc;
+  xdg.configFile."fastfetch/config.jsonc" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "/home/zno/zno-config/home/laptop/de/fastfetch/config.jsonc";
+    force = true;
+  };
+  xdg.configFile."fastfetch/ascii".source = ./ascii;
+  xdg.configFile."fastfetch/png".source = ./png;
 }
