711e83a84a19563a7cf4c5254103ade8f1f24a5a
zno233 <zno233@outlook.com>
2026-01-17 17:11:33 +0800

    change font config

--- modules/system/nixos/core/font.nix
+++ modules/system/nixos/core/font.nix
@@ -6,20 +6,55 @@
       antialias = true;
       hinting = {
         enable = true;
-        style = "slight"; # 可尝试 "medium" 获得更强清晰度
+        style = "slight";
         # autohint = true;
       };
-      subpixel = {
-        rgba = "rgb";
-        lcdfilter = "default";
-      };
+      # subpixel = {
+      #   rgba = "rgb";
+      #   lcdfilter = "light";
+      # };
       # useEmbeddedBitmaps = true;
 
       defaultFonts = {
-        sansSerif = [ "Noto Sans CJK SC" "wqy-microhei" ];
-        serif = [ "lxgw-wenkai" "Noto Serif CJK SC" "Noto Serif" ];
-        monospace = [ "Hurmit Nerd Font Mono" "Noto Sans Mono CJK SC" ];
+        # 西文: 衬线字体（笔画末端有修饰(衬线)的字体，通常用于印刷。）
+        # 中文: 宋体（港台称明體）
+        serif = [
+          "LXGW WenKai Screen"
+          "Noto Serif CJK SC"
+          "Noto Serif"
+        ];
+
+        # 西文: 无衬线字体（指笔画末端没有修饰(衬线)的字体，通常用于屏幕显示）
+        # 中文: 黑体
+        sansSerif = [
+          "Noto Sans CJK SC"
+          "Noto Sans" 
+        ];
+
+        # 等宽字体
+        monospace = [
+          "Maple mono"
+          "Sarasa Mono SC"
+          "Hurmit Nerd Font Mono"
+        ];
+
+        emoji = [ 
+          "Noto Color Emoji"
+        ];
       };
+
+      # 高 DPI下最好显式关闭stem darkening
+      localConf = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+      <fontconfig>
+        <match target="font">
+          <edit name="stem-darkening" mode="assign">
+            <bool>false</bool>
+          </edit>
+        </match>
+      </fontconfig>
+      '';
     };
 
     enableDefaultPackages = true;
@@ -32,14 +67,9 @@
       noto-fonts-emoji-blob-bin
       nerd-fonts.hurmit
       lxgw-wenkai
-      wqy_zenhei
-      wqy_microhei
+      lxgw-wenkai-screen
       sarasa-gothic
-      source-han-sans
-      source-han-serif
       #joypixels
     ];
   };
 }
-
-
